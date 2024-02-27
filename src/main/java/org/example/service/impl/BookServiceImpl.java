a17f23d4a5ac9f7c217897d374152fd1d4f3e427
Dammika-Pushpakumara <ivdammika@gmail.com>
2024-02-27 12:03:57 +0530

    autowired BookRepository interface

--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -1,11 +1,15 @@
 package org.example.service.impl;
 
 import org.example.dto.Book;
+import org.example.repository.BookRepository;
 import org.example.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    BookRepository bookRepository;
     @Override
     public void addBook(Book book) {
 
